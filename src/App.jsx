2f502ed9e85b93a77d9f4c0d95e6d9d113fd7d55
Bhanuteja74 <145745009+Bhanuteja74@users.noreply.github.com>
2025-07-06 21:55:19 +0530

    Defining Routes  here Link component - it updates the URL and triggers routing.  Routes - holds all the routes.  Route maps the url using path and element

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
 import { Suspense, lazy, useState } from "react";
 import "./App.css";
 import { Home } from "./Home";
+import { Link, Route, Routes } from "react-router-dom";
 
 const About = lazy(() => import("./About"));
 const Contact = lazy(() => import("./Contact"));
 
 function App() {
-  const [showAbout, setShowAbout] = useState(false);
-  const [showContact, setShowContact] = useState(false);
-
   return (
     <>
-      <button onClick={() => setShowAbout(true)}>Show About</button>
-      <button onClick={() => setShowContact(true)}>Show Contact</button>
-      <Home />
-      {showAbout && (
-        <Suspense fallback={<div>Loading About Page...</div>}>
-          <About />
-        </Suspense>
-      )}
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
 
-      {showContact && (
-        <Suspense fallback={<div>Loading Contact Page...</div>}>
-          <Contact />
-        </Suspense>
-      )}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
