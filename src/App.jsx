1895dc68eb734c8183e3b7fbef75c49b552eea1c
Bhanuteja74 <145745009+Bhanuteja74@users.noreply.github.com>
2025-07-06 17:01:59 +0530

    creates contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { Suspense, lazy, useState } from "react";
-const About = lazy(() => import("./About"));
 import "./App.css";
 import { Home } from "./Home";
 
+const About = lazy(() => import("./About"));
+const Contact = lazy(() => import("./Contact"));
+
 function App() {
   const [showAbout, setShowAbout] = useState(false);
+  const [showContact, setShowContact] = useState(false);
 
   return (
     <>
       <button onClick={() => setShowAbout(true)}>Show About</button>
+      <button onClick={() => setShowContact(true)}>Show Contact</button>
       <Home />
       {showAbout && (
         <Suspense fallback={<div>Loading...</div>}>
           <About />
+        </Suspense>
+      )}
+
+      {showContact && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Contact />
         </Suspense>
       )}
     </>
---
+++ src/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <>
+      <h1>Contact Page</h1>
+      <p>Here you can find contact details</p>
+    </>
+  );
+}
