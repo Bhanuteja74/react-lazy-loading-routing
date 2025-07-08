ba7795841a99c86b6b46c0fe3099a1d818703a80
Bhanuteja74 <145745009+Bhanuteja74@users.noreply.github.com>
2025-07-08 22:49:52 +0530

    Practices dynamic routes and url parameters. Adds one more component User

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Suspense, lazy } from "react";
 import "./App.css";
 import { Home } from "./Home";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useParams } from "react-router-dom";
 const About = lazy(() => import("./About"));
 const Contact = lazy(() => import("./Contact"));
 const Dashboard = lazy(() => import("./Dashboard"));
 const NotFound = lazy(() => import("./NotFound"));
+
+function User() {
+  const { id } = useParams();
+  return <h1>User ID: {id}</h1>;
+}
 
 function App() {
   return (
@@ -15,6 +20,8 @@ function App() {
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/dashboard">Dashboard</Link>
+        <Link>Fake</Link>
+        <Link to="/user/101">User 101</Link>
       </nav>
 
       <Suspense fallback={<div>Loading...</div>}>
@@ -22,6 +29,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/user/:id" element={<User />} />
+
           <Route path="/dashboard" element={<Dashboard />}>
             <Route path="settings" element={<div>Settings Page</div>} />
             <Route path="profile" element={<div>Profile Page</div>} />
