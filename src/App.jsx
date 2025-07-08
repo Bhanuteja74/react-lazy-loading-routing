89e128f2cd56f1b309dd436a272a5753ae09ae03
Bhanuteja74 <145745009+Bhanuteja74@users.noreply.github.com>
2025-07-08 23:32:04 +0530

    Creates Component for protect the route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Suspense, lazy } from "react";
 import "./App.css";
 import { Home } from "./Home";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 const About = lazy(() => import("./About"));
 const Contact = lazy(() => import("./Contact"));
 const Dashboard = lazy(() => import("./Dashboard"));
 const NotFound = lazy(() => import("./NotFound"));
 const User = lazy(() => import("./User"));
+
+function ProtectedRoute({ children }) {
+  const isAuthenticated = false;
+  return isAuthenticated ? children : <Navigate to="/" />;
+}
 
 function App() {
   return (
@@ -27,7 +32,14 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/user/:id" element={<User />} />
 
-          <Route path="/dashboard" element={<Dashboard />}>
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          >
             <Route path="settings" element={<div>Settings Page</div>} />
             <Route path="profile" element={<div>Profile Page</div>} />
           </Route>
